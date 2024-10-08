8240f7587dc76821d77bc88deb709369618d8286
jimmy <wjdtjq6@gmail.com>
2024-10-08 11:31:37 +0900

    init

---
+++ PbxConflict/PbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PbxConflictApp.swift
+//  PbxConflict
+//
+//  Created by 소정섭 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
